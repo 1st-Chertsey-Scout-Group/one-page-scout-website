c492f7121628cdb845dc1a326d3ed57447311810
Tim Cane <3074314+TimCane@users.noreply.github.com>
2024-11-12 22:12:32 +0000

    Initial (#1)
    * added initial version using chertsey data
    
    * initial implementation of the group site template

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from "astro:content";
+
+const sectionsCollection = defineCollection({
+    type: "data",
+    schema: ({ image }) =>
+        z.object({
+            name: z.string(),
+            type: z.enum(["squirrels", "beavers", "cubs", "scouts"]),
+            meetingDay: z.enum(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]),
+            age: z.string(),
+            meetingStartTime: z.string(),
+            meetingEndTime: z.string(),
+            order: z.number(),
+            image: image()
+        }),
+});
+
+export const collections = {
+    sections: sectionsCollection,
+};
